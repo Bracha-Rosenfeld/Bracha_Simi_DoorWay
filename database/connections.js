e5bbc132682a4138a857237d64034c25faa0c42b
Bracha-Rosenfeld <Hand4d28@mbjcomp.org.il>
2025-05-29 12:49:47 +0300

    created the database and insrted all the relevent tables

---
+++ database/connections.js
@@ -0,0 +1,18 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
+
+export var con = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    // database: process.env.MYSQL_DATABASE
+});
+
+con.connect(function(err) {
+    if (err) {
+        console.error('Error connecting to MySQL database:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
---
+++ database/tables.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE IF NOT EXISTS mydatabase;
+USE mydatabase;
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50),
+  email VARCHAR(100),
+  phone VARCHAR(255) UNIQUE,
+  address VARCHAR(255),
+  longitude DOUBLE,
+  latitude DOUBLE
+);
+
+CREATE TABLE IF NOT EXISTS roles (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  rolename VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS user_roles (
+  user_id INT,
+  role_id INT,
+  PRIMARY KEY (user_id, role_id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
+CREATE TABLE IF NOT EXISTS passwords (
+  user_id INT PRIMARY KEY,
+  password VARCHAR(255),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS apartments(
+	id INT PRIMARY KEY,
+    publisher_id INT,
+    address VARCHAR(255),
+    num_of_rooms DECIMAL(3,1),
+    area DOUBLE,
+    floor_number INT,
+    details TEXT,
+    FOREIGN KEY (publisher_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS images(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    apartment_id INT,
+    image_url VARCHAR(255),
+    FOREIGN KEY (apartment_id) REFERENCES apartments(id)
+);
+
