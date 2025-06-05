48ccbbd6a9ba2c410ea452bd451e1a16c9163817
Bracha-Rosenfeld <Hand4d28@mbjcomp.org.il>
2025-06-05 17:50:53 +0300

    I added the logo on the nav bar

--- client/src/components/navBar/navBar.jsx
+++ client/src/components/navBar/navBar.jsx
@@ -5,8 +5,8 @@ import styles from './NavBar.module.css';
 const NavBar = () => {
     return (
         <div className={styles.wrapper}>
-            <nav className={styles.navbar}>
-                <h2 className={styles.logo}>🏠The DoorWay</h2>
+            <nav className={styles.navbar}>                
+                <h2 className={styles.logo}><img src="/doorway_logo_custom.svg" alt="Doorway Logo" className="logo" /></h2>
                 <ul className={styles.navList}>
                     <li className={styles.navItem}><Link to="/about" className={styles.navLink}>About</Link></li>
                     <li className={styles.navItem}><Link to="/deals" className={styles.navLink}>Deals</Link></li>
