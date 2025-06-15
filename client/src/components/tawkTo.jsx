b4ff1338f9cc50e88f0d4c045cdeb09ed399e2e5
Sima-Shulman <Hand4d31@mbjcomp.org.il>
2025-06-15 11:39:37 +0300

    Change the account in tawk.to. Change the comonent tawkTo

--- client/src/components/tawkTo.jsx
+++ client/src/components/tawkTo.jsx
@@ -1,24 +1,21 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 
-const TawkTo = () => {
+const TawkToWidget = () => {
   useEffect(() => {
-    var Tawk_API = window.Tawk_API || {};
-    var Tawk_LoadStart = new Date();
-
     if (!document.getElementById('tawk-script')) {
-      var s1 = document.createElement('script');
+      const s1 = document.createElement('script');
       s1.id = 'tawk-script';
       s1.async = true;
-      s1.src = 'https://embed.tawk.to/684e31c99a4b03190ad925c5/1iton4uuk';
+      s1.src = 'https://embed.tawk.to/684e8189603cf9190a79a856/1itpak0ne';
       s1.charset = 'UTF-8';
       s1.setAttribute('crossorigin', '*');
 
-      var s0 = document.getElementsByTagName('script')[0];
+      const s0 = document.getElementsByTagName('script')[0];
       s0.parentNode.insertBefore(s1, s0);
     }
   }, []);
 
-  return null; // אין צורך להחזיר JSX כי הסקריפט נטען במציאות DOM
+  return null;
 };
 
-export default TawkTo;
+export default TawkToWidget;
