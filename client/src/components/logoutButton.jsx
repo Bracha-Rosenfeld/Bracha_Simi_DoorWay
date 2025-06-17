51ba2c94643e519ac8504cb6a85749fbb6e8e7a5
Bracha-Rosenfeld <Hand4d28@mbjcomp.org.il>
2025-06-17 10:37:24 +0300

    design for logout button

--- client/src/components/logoutButton.jsx
+++ client/src/components/logoutButton.jsx
@@ -1,6 +1,28 @@
-// Example: Add this function to any component where you want to allow logout
+// // Example: Add this function to any component where you want to allow logout
+// import { useCurrentUser } from './userProvider';
+// import { useNavigate } from 'react-router-dom';
+
+// function LogoutButton() {
+//   const { setCurrentUser } = useCurrentUser();
+//   const navigate = useNavigate();
+
+//   const handleLogout = async () => {
+//     await fetch('http://localhost:5000/users/logout', {
+//       method: 'POST',
+//       credentials: 'include'
+//     });
+//     setCurrentUser(null);
+//     navigate('/login');
+//   };
+
+//   return <button onClick={handleLogout}>Logout</button>;
+// }
+
+// export default LogoutButton;
+
 import { useCurrentUser } from './userProvider';
 import { useNavigate } from 'react-router-dom';
+import styles from './userAccount.module.css';
 
 function LogoutButton() {
   const { setCurrentUser } = useCurrentUser();
@@ -15,7 +37,14 @@ function LogoutButton() {
     navigate('/login');
   };
 
-  return <button onClick={handleLogout}>Logout</button>;
+  return (
+    <button 
+      className={styles.logoutButton} 
+      onClick={handleLogout}
+    >
+      Logout
+    </button>
+  );
 }
 
 export default LogoutButton;
